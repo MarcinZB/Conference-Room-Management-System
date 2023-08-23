c11cebcf76eb0ce7c433dd7d3072a4b6e474387b
MarcinZB <marcin.zbroszczyk@proton.me>
2023-08-23 17:29:30 +0200

    Create site with possibility to add new room

--- Reservation_app/Reservation_app/urls.py
+++ Reservation_app/Reservation_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reservation_app.views import AddRoomView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('room/new/', AddRoomView.as_view(), name="add-room"),
 ]
--- Reservation_app/reservation_app/views.py
+++ Reservation_app/reservation_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import ConferenceRoom
+
 
 # Create your views here.
+
+class AddRoomView(View):
+    def get(self, request):
+        return render(request, "add_room.html")
+
+    def post(self, request):
+        name = request.POST.get("room-name")
+        capacity = request.POST.get("capacity")
+        capacity = int(capacity) if capacity else 0
+        projector = request.POST.get("projector") == "on"
+
+        if not name:
+            return render(request, "add_room.html", context={"error": "Nie podano nazwy sali"})
+        if capacity <= 0:
+            return render(request, "add_room.html", context={"error": "Pojemność musi być dodatnia"})
+        if ConferenceRoom.objects.filter(name=name).first():
+            return render(request, "add_room.html", context={"error": "Sala o podanej nazwie istnieje"})
+
+        ConferenceRoom.object.create(name=name, capacity=capacity, projector_avaliability=projector)
+        return redirect("room-list")
