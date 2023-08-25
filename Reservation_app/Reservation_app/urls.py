a1d616562ab673a057a4855c0d5e48d9658dd8dc
MarcinZB <marcin.zbroszczyk@proton.me>
2023-08-25 16:56:20 +0200

    Possibility to modify room added

--- Reservation_app/Reservation_app/urls.py
+++ Reservation_app/Reservation_app/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from reservation_app.views import AddRoomView, RoomListView, DeleteRoomView
+from reservation_app.views import AddRoomView, RoomListView, DeleteRoomView, ModifyRoomView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('room/new/', AddRoomView.as_view(), name="add-room"),
     path('', RoomListView.as_view(), name="room-list"),
     path('room/delete/<int:room_id>/', DeleteRoomView.as_view(), name="delete-room"),
+    path('room/modify/<int:room_id>/', ModifyRoomView.as_view(), name="modify-room"),
 ]
